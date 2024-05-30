a899757d2dbe9ff47274f67562981a5ce1ccf081
zjzhang <zhangzj33@gmail.com>
2024-05-30 16:48:20 +0800

    update textfield

--- widget/input.go
+++ widget/input.go
@@ -21,7 +21,8 @@ import (
 	"github.com/oligo/gioview/theme"
 )
 
-type state int
+type state uint8
+type LabelAlignment uint8
 
 const (
 	inactive state = iota
@@ -30,7 +31,25 @@ const (
 	focused
 )
 
-// Another TextField implementation
+const (
+	Top LabelAlignment = iota
+	Left
+	Right
+	Hidden
+)
+
+type LabelOption struct {
+	Alignment LabelAlignment
+	Ratio     float32
+	// Space between laben and input box
+	Padding unit.Dp
+}
+
+// Another TextField implementation with the following features:
+// * configurable padding and border radius
+// * more compact design by put character counters inline.
+// * subscribe ESC key events to defocus the text field.
+// * configurable label alignment.
 type TextField struct {
 	// padding between the text and border.
 	Padding unit.Dp
@@ -39,23 +58,28 @@ type TextField struct {
 	SingleLine bool
 	// Text alignment in the box.
 	Alignment text.Alignment
+	// Label alignment option
+	LabelOption LabelOption
 
 	// Helper text to give additional context to a field.
 	HelperText string
 	// The maximum number of characters the text input will allow.
 	// Zero means no limit.
 	MaxChars int
+	// Mask replaces the visual display of each rune in the contents with the given rune.
+	Mask rune
 
 	// Leading appears before the content of the text input.
 	Leading layout.Widget
 	// Trailing appears after the content of the text input.
 	Trailing layout.Widget
-	Editor   widget.Editor
+	// Prefer not to export editor to users.
+	editor widget.Editor
 
 	// click detects when the mouse pointer clicks or hovers
 	// within the textfield.
 	click gesture.Click
-	// cached text from the Editor, updated when editor content is changed
+	// cached text from the editor, updated when editor content is changed
 	text   string
 	border border
 
@@ -70,29 +94,44 @@ type border struct {
 	Color     color.NRGBA
 }
 
-func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
+func (in *TextField) init() {
 	if in.Radius == 0 {
 		in.Radius = unit.Dp(4)
 	}
 	if in.Padding == 0 {
-		in.Padding = unit.Dp(8)
+		in.Padding = unit.Dp(12)
 	}
 	if in.MaxChars < 0 {
 		in.MaxChars = 0
 	}
 
-	if in.SingleLine != in.Editor.SingleLine {
-		in.Editor.SingleLine = in.SingleLine
+	if in.LabelOption.Ratio <= 0 {
+		in.LabelOption.Ratio = 0.25
+	}
+	if in.LabelOption.Padding <= 0 {
+		in.LabelOption.Padding = unit.Dp(12)
 	}
 
-	if in.Alignment != in.Editor.Alignment {
-		in.Editor.Alignment = in.Alignment
+	if in.SingleLine != in.editor.SingleLine {
+		in.editor.SingleLine = in.SingleLine
 	}
 
-	if in.MaxChars != in.Editor.MaxLen {
-		in.Editor.MaxLen = in.MaxChars
+	if in.Alignment != in.editor.Alignment {
+		in.editor.Alignment = in.Alignment
 	}
 
+	if in.MaxChars != in.editor.MaxLen {
+		in.editor.MaxLen = in.MaxChars
+	}
+	if in.Mask != in.editor.Mask {
+		in.editor.Mask = in.Mask
+	}
+
+	// Enable submit by default.
+	in.editor.Submit = true
+}
+
+func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 	disabled := gtx.Source == (input.Source{})
 	for {
 		ev, ok := in.click.Update(gtx.Source)
@@ -101,7 +140,7 @@ func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 		}
 		switch ev.Kind {
 		case gesture.KindPress:
-			gtx.Execute(key.FocusCmd{Tag: &in.Editor})
+			gtx.Execute(key.FocusCmd{Tag: &in.editor})
 		}
 	}
 
@@ -109,10 +148,10 @@ func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 	if in.click.Hovered() && !disabled {
 		in.state = hovered
 	}
-	if in.Editor.Len() > 0 {
+	if in.editor.Len() > 0 {
 		in.state = activated
 	}
-	if gtx.Source.Focused(&in.Editor) && !disabled {
+	if gtx.Source.Focused(&in.editor) && !disabled {
 		in.state = focused
 	}
 
@@ -141,7 +180,7 @@ func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 
 	// Update text if editor content is changed or editor is submitted.
 	for {
-		event, ok := in.Editor.Update(gtx)
+		event, ok := in.editor.Update(gtx)
 		if !ok {
 			break
 		}
@@ -154,13 +193,13 @@ func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 			continue
 		}
 		in.changed = true
-		in.text = in.Editor.Text()
+		in.text = in.editor.Text()
 		break
 	}
 
 	// Catch the ESC key being pressed to release the widget's focus.
 	for {
-		evt, ok := gtx.Event(key.Filter{Focus: &in.Editor, Name: key.NameEscape})
+		evt, ok := gtx.Event(key.Filter{Focus: &in.editor, Name: key.NameEscape})
 		if !ok {
 			break
 		}
@@ -176,6 +215,7 @@ func (in *TextField) update(gtx layout.Context, th *theme.Theme) {
 }
 
 func (in *TextField) Layout(gtx layout.Context, th *theme.Theme, hint string) layout.Dimensions {
+	in.init()
 	in.update(gtx, th)
 	gtx.Constraints.Min.X = gtx.Constraints.Max.X
 	gtx.Constraints.Min.Y = 0
@@ -187,16 +227,70 @@ func (in *TextField) Layout(gtx layout.Context, th *theme.Theme, hint string) la
 	defer pointer.PassOp{}.Push(gtx.Ops).Pop()
 	defer clip.Rect(image.Rectangle{Max: dims.Size}).Push(gtx.Ops).Pop()
 	in.click.Add(gtx.Ops)
-	event.Op(gtx.Ops, &in.Editor)
+	event.Op(gtx.Ops, &in.editor)
 	call.Add(gtx.Ops)
 	return dims
 }
 
 func (in *TextField) layout(gtx layout.Context, th *theme.Theme, hint string) layout.Dimensions {
+	if in.LabelOption.Alignment != Left && in.LabelOption.Alignment != Right {
+		return in.layout2(gtx, th, hint)
+	}
 
+	weight := in.LabelOption.Ratio
+	if hint == "" {
+		weight = 0
+	}
+
+	var direction layout.Direction
+	if in.LabelOption.Alignment == Left {
+		direction = layout.W
+	} else if in.LabelOption.Alignment == Right {
+		direction = layout.E
+	}
+
+	return layout.Flex{
+		Axis:      layout.Horizontal,
+		Alignment: layout.Middle,
+	}.Layout(gtx,
+		layout.Flexed(weight, func(gtx C) D {
+			if hint == "" {
+				return D{}
+			}
+
+			return direction.Layout(gtx, func(gtx C) D {
+				label := material.Label(th.Theme, th.TextSize, hint)
+				// label.Color = in.border.Color
+				return label.Layout(gtx)
+			})
+
+		}),
+		layout.Rigid(layout.Spacer{Width: in.LabelOption.Padding}.Layout),
+		layout.Flexed(1-weight, func(gtx C) D {
+			return in.layout2(gtx, th, hint)
+		}),
+	)
+}
+
+func (in *TextField) layout2(gtx layout.Context, th *theme.Theme, hint string) layout.Dimensions {
 	return layout.Flex{
 		Axis: layout.Vertical,
 	}.Layout(gtx,
+		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+			if hint == "" || in.LabelOption.Alignment != Top {
+				return layout.Dimensions{}
+			}
+
+			return layout.Inset{
+				Bottom: unit.Dp(4),
+			}.Layout(gtx, func(gtx C) D {
+				label := material.Label(th.Theme, th.TextSize*0.8, hint)
+				label.Color = in.border.Color
+				return label.Layout(gtx)
+			})
+
+		}),
+
 		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 			border := widget.Border{
 				Color:        in.border.Color,
@@ -217,10 +311,30 @@ func (in *TextField) layout(gtx layout.Context, th *theme.Theme, hint string) la
 							return layout.Dimensions{}
 						}),
 						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-							return material.Editor(th.Theme, &in.Editor, hint).Layout(gtx)
+							return material.Editor(th.Theme, &in.editor, hint).Layout(gtx)
 						}),
 						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-							if in.Trailing != nil && in.state == activated {
+							if in.MaxChars <= 0 {
+								return layout.Dimensions{}
+							}
+							return layout.Inset{
+								Left: in.Padding,
+							}.Layout(
+								gtx,
+								func(gtx layout.Context) layout.Dimensions {
+									count := material.Label(
+										th.Theme,
+										th.TextSize*0.9,
+										strconv.Itoa(in.editor.Len())+"/"+strconv.Itoa(int(in.MaxChars)),
+									)
+									count.Color = misc.WithAlpha(th.Fg, 128)
+									return count.Layout(gtx)
+								},
+							)
+						}),
+
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							if in.Trailing != nil && in.state >= activated {
 								return layout.Inset{Left: in.Padding}.Layout(gtx, in.Trailing)
 							}
 							return layout.Dimensions{}
@@ -230,56 +344,27 @@ func (in *TextField) layout(gtx layout.Context, th *theme.Theme, hint string) la
 			})
 		}),
 		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			return layout.Flex{
-				Axis:      layout.Horizontal,
-				Alignment: layout.Middle,
-				Spacing:   layout.SpaceBetween,
+			if in.HelperText == "" && in.errorMsg == "" {
+				return layout.Dimensions{}
+			}
+
+			helper := in.errorMsg
+			helperColor := color.NRGBA{R: 200, A: 255}
+			if helper == "" {
+				helper = in.HelperText
+				helperColor = misc.WithAlpha(th.Fg, 128)
+			}
+
+			return layout.Inset{
+				Top:  unit.Dp(4),
+				Left: unit.Dp(10),
 			}.Layout(
 				gtx,
-				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					if in.HelperText == "" && in.errorMsg == "" {
-						return layout.Dimensions{}
-					}
-
-					helper := in.errorMsg
-					helperColor := color.NRGBA{R: 200, A: 255}
-					if helper == "" {
-						helper = in.HelperText
-						helperColor = misc.WithAlpha(th.Fg, 128)
-					}
-
-					return layout.Inset{
-						Top:  unit.Dp(4),
-						Left: unit.Dp(10),
-					}.Layout(
-						gtx,
-						func(gtx layout.Context) layout.Dimensions {
-							helper := material.Label(th.Theme, th.TextSize*0.9, helper)
-							helper.Color = helperColor
-							return helper.Layout(gtx)
-						},
-					)
-				}),
-				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					if in.MaxChars <= 0 {
-						return layout.Dimensions{}
-					}
-					return layout.Inset{
-						Top:   unit.Dp(4),
-						Right: unit.Dp(10),
-					}.Layout(
-						gtx,
-						func(gtx layout.Context) layout.Dimensions {
-							count := material.Label(
-								th.Theme,
-								th.TextSize*0.9,
-								strconv.Itoa(in.Editor.Len())+"/"+strconv.Itoa(int(in.MaxChars)),
-							)
-							count.Color = misc.WithAlpha(th.Fg, 128)
-							return count.Layout(gtx)
-						},
-					)
-				}),
+				func(gtx layout.Context) layout.Dimensions {
+					helper := material.Label(th.Theme, th.TextSize*0.9, helper)
+					helper.Color = helperColor
+					return helper.Layout(gtx)
+				},
 			)
 		}),
 	)
@@ -287,14 +372,14 @@ func (in *TextField) layout(gtx layout.Context, th *theme.Theme, hint string) la
 }
 
 func (in *TextField) Focused(gtx layout.Context) bool {
-	return gtx.Focused(in.Editor)
+	return gtx.Focused(in.editor)
 }
 
 // Clear clears the input text.
 func (in *TextField) Clear() {
 	in.changed = true
 	in.text = ""
-	in.Editor.SetText("")
+	in.editor.SetText("")
 	in.errorMsg = ""
 }
 
@@ -306,7 +391,7 @@ func (in *TextField) Text() string {
 func (in *TextField) SetText(text string) {
 	in.changed = true
 	in.text = text
-	in.Editor.SetText(text)
+	in.editor.SetText(text)
 	in.errorMsg = ""
 }
 
