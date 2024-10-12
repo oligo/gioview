8c2332f3c95d2c8d3756d0b84d6f98b58e5c8220
zjzhang <zhangzj33@gmail.com>
2024-10-13 01:51:50 +0800

    refactor Tag API

--- widget/tag.go
+++ widget/tag.go
@@ -25,43 +25,43 @@ const (
 // Tag is used for items that need to be labeled using keywords
 // that describe them.
 type Tag struct {
-	TextColor  color.NRGBA
+	Text     string
+	TextSize unit.Sp
+	Font     font.Font
+	// Text color the of label. For outine variant, this is also the border of the tag.
+	TextColor color.NRGBA
+	// Background color of the label. Only valid in the case of Solid variant.
 	Background color.NRGBA
 	Radius     unit.Dp
 	Inset      layout.Inset
 	Variant    TagVariant
 }
 
-func (t Tag) Layout(gtx C, th *theme.Theme, textSize unit.Sp, txt string) D {
+func (t Tag) Layout(gtx C, th *theme.Theme) D {
 	textColorMacro := op.Record(gtx.Ops)
 	paint.ColorOp{Color: t.TextColor}.Add(gtx.Ops)
 	textColor := textColorMacro.Stop()
 
-	textFont := font.Font{
-		Typeface: th.Face,
-		Weight:   font.Normal,
+	textFont := t.Font
+	if t.Font == (font.Font{}) {
+		textFont = font.Font{
+			Typeface: th.Face,
+			Weight:   font.Normal,
+		}
 	}
 
 	switch t.Variant {
 	case Outline:
-		return widget.Border{
-			Color:        t.TextColor,
-			CornerRadius: t.Radius,
-			Width:        gtx.Metric.PxToDp(1),
-		}.Layout(gtx, func(gtx C) D {
-			return t.Inset.Layout(gtx, func(gtx C) D {
-				return t.layoutText(gtx, th.Shaper, textFont, th.TextSize, textColor, txt)
-			})
-		})
-
+		return t.layoutOutline(gtx, th.Shaper, textFont, textColor)
 	case Solid:
-		return t.layoutSolid(gtx, th.Shaper, textFont, th.TextSize, textColor, txt)
+		return t.layoutSolid(gtx, th.Shaper, textFont, textColor)
 	}
 
 	return D{}
 }
 
 func (t Tag) layoutText(gtx C, shaper *text.Shaper, font font.Font, size unit.Sp, textMaterial op.CallOp, txt string) D {
+	gtx.Constraints.Min.X = 0
 
 	tl := widget.Label{
 		Alignment: text.Start,
@@ -71,10 +71,10 @@ func (t Tag) layoutText(gtx C, shaper *text.Shaper, font font.Font, size unit.Sp
 	return tl.Layout(gtx, shaper, font, size, txt, textMaterial)
 }
 
-func (t Tag) layoutSolid(gtx C, shaper *text.Shaper, font font.Font, size unit.Sp, textMaterial op.CallOp, txt string) D {
+func (t Tag) layoutSolid(gtx C, shaper *text.Shaper, font font.Font, textMaterial op.CallOp) D {
 	macro := op.Record(gtx.Ops)
 	dims := t.Inset.Layout(gtx, func(gtx C) D {
-		return t.layoutText(gtx, shaper, font, size, textMaterial, txt)
+		return t.layoutText(gtx, shaper, font, t.TextSize, textMaterial, t.Text)
 	})
 	callOps := macro.Stop()
 
@@ -83,4 +83,16 @@ func (t Tag) layoutSolid(gtx C, shaper *text.Shaper, font font.Font, size unit.S
 	callOps.Add(gtx.Ops)
 
 	return dims
+}
+
+func (t Tag) layoutOutline(gtx C, shaper *text.Shaper, font font.Font, textMaterial op.CallOp) D {
+	return widget.Border{
+		Color:        t.TextColor,
+		CornerRadius: t.Radius,
+		Width:        unit.Dp(1),
+	}.Layout(gtx, func(gtx C) D {
+		return t.Inset.Layout(gtx, func(gtx C) D {
+			return t.layoutText(gtx, shaper, font, t.TextSize, textMaterial, t.Text)
+		})
+	})
 }
